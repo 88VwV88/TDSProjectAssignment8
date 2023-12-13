0e1d014c58f9619e67ca76e35a729df0ae34939a
VwV <dhimankkvivek773@gmail.com>
2023-12-13 23:39:59 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
 st.write('''
-# My first app
-Hello *world!*
+# Greatest of three numbers: 
 ''')
+
+st.write('''### Please enter 3 numbers: ''')
+
+a = st.text_input('a')
+b = st.text_input('b')
+c = st.text_input('c')
+if st.button('Submit'):
+    a, b, c = int(a), int(b), int(c)
+    st.write(f'''The greatest number is: {max(a, b, c)}''')
+else:
+    st.write('''*Please enter valid numbers!*''')
